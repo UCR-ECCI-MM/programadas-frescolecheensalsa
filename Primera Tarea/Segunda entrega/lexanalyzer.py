0c6118a4f81662eaba36b3a660dc9b4f36c4af14
kdaniel1652 <kennethvillalobos16@gmail.com>
2023-03-25 20:01:35 -0600

    Added regex functions for the date and url

--- Primera Tarea/Segunda entrega/lexanalyzer.py
+++ Primera Tarea/Segunda entrega/lexanalyzer.py
@@ -11,6 +11,22 @@ import ply.lex as lex
 
 # ------------------  Code for ply -------------------------------------
 
+# A regular expression rules with some action code
+# Define a rule to handle the dates
+def t_DATE(token):
+    # DD-MM-YYYY (01 <= DD <= 31; 01 <= MM <= 12; 1950 <= YYYY <= 2023)
+    # D-M-YYYY
+    r'((0*[1-9])|([12][0-9])|(3[01]))\-(0*[1-9]|1[0-2])\-(19[5-9][0-9]|20([01][0-9]|2[0-3]))'
+    return token
+
+# Define a rule to handle URL´s
+def t_URL(token):
+    # TODO(Kenneth): Ask and Alejandro and Joseph about this regex and its
+    # repercussions, if necessary, change it
+    # Https:// (optional) www. (optional) something with letters or numbers . something that does not have space
+    r'([a-z]+\:\/\/)?(www\.)?((\w|-)+\.[^ <>]+)'
+    return token
+
 # Define a rule so we can track line numbers
 def t_newline(token):
     r'\n+'
