415c8a8b81247f4c2867ddb47fa0f4aa02b7acd4
Alejandro Jimenez Corea <pichicareto24@gmail.com>
2023-07-03 15:34:32 -0600

    BinPacking.hpp was added to the repository

---
+++ Segunda Tarea/Segunda entrega/BinPacking.hpp
@@ -0,0 +1,57 @@
+#ifndef BINPACKING_HPP
+#define BINPACKING_HPP
+
+#include "common.hpp"
+
+namespace FrescolecheEnSalsa {
+  std::shared_ptr<solutionT> binPacking(
+      const FrescolecheEnSalsa::cookieRestrictions& restrictions,
+      const std::vector<size_t>& cookieAmounts // amount of each cookie backed
+    ) {
+
+    // check if sizes are consistent, if otherwise
+    if (restrictions.cookieCosts.size() != cookieAmounts.size()) {
+      // exit and report on error
+      std::cerr <<  "Error: Inconsistent number of cookie types" << std::endl;
+      return nullptr;
+    }
+
+    // get an vector of all cokies with the cookie amounts
+    Cookies cookies(cookieAmounts);
+
+    // get a list to place the solution
+    std::shared_ptr<solutionT> solution = std::make_shared<solutionT>();
+
+    // for all cookies
+    for (cookieT cookie : cookies) {
+      bool placed = false;
+
+      // find a bin that can fit the item
+      for (packetT& packet : *solution) {
+        // if it fits
+        if (canAdd(packet, restrictions, cookie)) {
+          // place inside
+          packet.push_back(cookie);
+
+          // marked placed as true
+          placed = true;
+
+          // exit loop
+          break;
+        }
+      }
+
+      // if none found
+      if (!placed) {
+        // get a new bin
+        solution->emplace_back(packetT());
+
+        // place inside just added bin
+        solution->back().push_back(cookie);
+      }
+    }
+
+    return solution;
+  }
+};
+#endif
