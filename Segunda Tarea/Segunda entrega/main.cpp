3d74bf734b1f5bbaa3fd5823e15ba9ba3b2df412
Alejandro Jimenez Corea <pichicareto24@gmail.com>
2023-07-05 17:46:21 -0600

    Se agregaron cambios al main

--- Segunda Tarea/Segunda entrega/main.cpp
+++ Segunda Tarea/Segunda entrega/main.cpp
@@ -16,38 +16,38 @@ int main(int argc, char* argv[]) {
   }
 
   std::vector<int> cookieCosts(6);
-  cookieCosts[simple] = 300;   // Simple
-  cookieCosts[chips] = 500;   // Chispas
-  cookieCosts[doubleChocolate] = 700;   // Doble chocolate
-  cookieCosts[oatmeal] = 400;   // Avena
-  cookieCosts[weed] = 2000;  // Weed
-  cookieCosts[vegan] = 1000;  // Vegana
-
-  //std::vector<size_t> cookieAmounts(6, 3);
+  cookieCosts[FrescolecheEnSalsa::SIMPLE] = 300;
+  cookieCosts[FrescolecheEnSalsa::CHIPS] = 500;
+  cookieCosts[FrescolecheEnSalsa::DOUBLE_CHOCOLATE] = 700;
+  cookieCosts[FrescolecheEnSalsa::OATMEAL] = 400;
+  cookieCosts[FrescolecheEnSalsa::WEED] = 2000;
+  cookieCosts[FrescolecheEnSalsa::VEGAN] = 1000;
 
   std::vector<size_t> cookieAmounts(6);
-  cookieAmounts[simple] = 3;   // Simple
-  cookieAmounts[chips] = 3;   // Chispas
-  cookieAmounts[doubleChocolate] = 3;   // Doble chocolate
-  cookieAmounts[oatmeal] = 3;   // Avena
-  cookieAmounts[weed] = 2;  // Weed
-  cookieAmounts[vegan] = 2;  // Vegana
+  cookieAmounts[FrescolecheEnSalsa::SIMPLE] = 2;
+  cookieAmounts[FrescolecheEnSalsa::CHIPS] = 2;
+  cookieAmounts[FrescolecheEnSalsa::DOUBLE_CHOCOLATE] = 2;
+  cookieAmounts[FrescolecheEnSalsa::OATMEAL] = 2;
+  cookieAmounts[FrescolecheEnSalsa::WEED] = 2;
+  cookieAmounts[FrescolecheEnSalsa::VEGAN] = 2;
 
-  FrescolecheEnSalsa::cookieRestrictions restrictions = {packetPrice, packetCapacity, cookieCosts};
+  FrescolecheEnSalsa::cookieRestrictions restrictions = {packetPrice,
+    packetCapacity, cookieCosts};
 
-  std::shared_ptr<FrescolecheEnSalsa::solutionT> solution;
+  std::shared_ptr<FrescolecheEnSalsa::Solution> solution;
 
   switch(choice) {
-    case algorithms::bruteForce:
+    case FrescolecheEnSalsa::algorithms::BRUTEFORCE:
       std::cout << "Brute Force solution" << std::endl;
       solution = FrescolecheEnSalsa::bruteForce(restrictions, cookieAmounts);
       break;
-    case algorithms::firstFit: 
+    case FrescolecheEnSalsa::algorithms::FIRSTFIT: 
       std::cout << "First Fit Bin Packing solution" << std::endl;
       solution = FrescolecheEnSalsa::binPacking(restrictions, cookieAmounts);
       break;
-    case algorithms::genetic: {
-      FrescolecheEnSalsa::GeneticAlgorithm solver(restrictions, cookieAmounts, 10);
+    case FrescolecheEnSalsa::algorithms::GENETIC: {
+      FrescolecheEnSalsa::GeneticAlgorithm solver(restrictions,
+        cookieAmounts);
       solution = solver.run();
       std::cout << "Genetic Algorithm solution" << std::endl;
       break;
