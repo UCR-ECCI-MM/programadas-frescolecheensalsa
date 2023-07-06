e3c67939879482c262451d68512a81af298438df
joseph.valverdekong <joseph.valverdekong@ucr.ac.cr>
2023-07-05 22:07:09 -0600

    Finished genetic algorithm functionalities

--- Segunda Tarea/Segunda entrega/GeneticAlgorithm.hpp
+++ Segunda Tarea/Segunda entrega/GeneticAlgorithm.hpp
@@ -111,6 +111,150 @@ namespace FrescolecheEnSalsa {
         this->population.pop_back();
       }
     }
-  };
 
+    void evolve() {
+      // Add three new solutions to the population by crossing:
+      // 1 and 2
+      // 2 and 3
+      // 3 and 4
+      for (size_t solution = 0; solution < 3; ++solution) {
+        this->population.push_back(this->cross(
+          this->population[solution].front(),
+          this->population[solution+1].front()));
+      }
+
+      // Sort the population from best to worst with the new solutions
+      std::sort(this->population.begin(), this->population.end());
+
+      // Delete the 3 worst solutions
+      for(size_t solution = 0; solution < 3; ++solution){
+        this->population.pop_back();
+      }
+    }
+
+    Solution cross(packetT genFirstParent,
+      packetT genSecondParent) {
+      // Generate a random number to simulate an event
+      const int event = rand() % 10;
+
+      // If the event is 2, then mutate
+      if (event == 2) {
+        // Change the packet of the first parent
+        // to a packet from a new solution to simulate
+        // a mutation
+        genFirstParent = this->generateSolution()[0];
+      }
+
+      // Get the remaining cookies
+      Cookies remainingCookies = std::move(
+        removeLeftovers(genFirstParent, genSecondParent));
+
+      // Create a solution to store the offspring
+      Solution offspring;
+
+      // Add the gens of the parents
+      offspring.push_back(genFirstParent);
+      offspring.push_back(genSecondParent);
+
+      // Shuffle the remaining cookies
+      std::shuffle(remainingCookies.begin(), remainingCookies.end(),
+        std::random_device());
+
+      for (cookieT cookie : remainingCookies) {
+        bool placed  = false;
+
+        for (packetT& packet : offspring) {
+          // If the cookie can be added to the packet
+          if (canAdd(packet, this->restrictions, cookie)) {
+            // Place the cookie inside the packet
+            packet.push_back(cookie);
+
+            // Mark placed as true
+            placed = true;
+
+            // Exit loop
+            break;
+          }
+        }
+
+        // if the cookie could not be placed
+        if (!placed) {
+          // Create a new packet
+          offspring.push_back(packetT());
+          // Add the cookie to the new packet
+          offspring.back().push_back(cookie);
+        }
+      }
+
+      // Set the net gain and the variability of the offspring
+      offspring.setnetGain(this->restrictions);
+      offspring.setVariablity();
+
+      // Return the offspring
+      return offspring;
+    }
+
+    Cookies removeLeftovers(packetT& genFirstParent,
+      packetT& genSecondParent) {
+      // get cookie amount for the first parent
+      std::vector<size_t> firstParentCookieCount =
+          countCookies(genFirstParent,
+          this->cookieAmounts.size());
+      // get cookie amount for the second parent
+      std::vector<size_t> secondParentCookieCount =
+          countCookies(genSecondParent,
+          this->cookieAmounts.size());
+
+      size_t availableCookies = 0;
+    
+      // remove any cookies that were placed but are above amounts
+      // for each cookie type
+      for (cookieT cookieType = 0; cookieType < this->cookieAmounts.size();
+        cookieType++) {
+        // get the amount of cookies availble for second parent
+        availableCookies =
+          this->cookieAmounts[cookieType] - firstParentCookieCount[cookieType];
+
+        // while there are more cookies than available in the second parent packet
+        while (secondParentCookieCount[cookieType] > availableCookies) {
+          // deduct the cookie from the packet
+          genSecondParent.erase(std::find(genSecondParent.begin(),
+              genSecondParent.end(), cookieType));
+          // reduce our amount counter for the given deducted cookie
+          --secondParentCookieCount[cookieType];
+        }
+      }
+
+      Cookies remainingCookies(this->totalCookies);
+
+      // for loop to remove the cookies from the first packet of the remaining
+      // cookies
+      for (cookieT cookie : genFirstParent) {
+        // find the cookie in the actual cookie count
+        packetT::iterator it = std::find(remainingCookies.begin(),
+          remainingCookies.end(), cookie);
+        // if in the actual cookie count
+        if (it != remainingCookies.end()) {
+          // remove the cookies from the actual cookie count
+          remainingCookies.erase(it);
+        }
+      }
+
+      // for loop to remove the cookies from the second packet of the remaining
+      // cookies
+      for (cookieT cookie : genSecondParent) {
+        // find the cookie in the actual cookie count
+        packetT::iterator it = std::find(remainingCookies.begin(),
+          remainingCookies.end(), cookie);
+        // if in the actual cookie count
+        if (it != remainingCookies.end()) {
+          // remove the cookies from the actual cookie count 
+          remainingCookies.erase(it);   
+        }
+      }
+
+      return remainingCookies;
+    }
+  };
+  
 };
